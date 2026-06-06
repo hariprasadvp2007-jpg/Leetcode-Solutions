2a3d25250635ecd077401646cbf75a7d514437bb
hariprasad <hariprasadvp2007@gmail.com>
2026-06-06 21:10:01 +0530

    Added Solution

---
+++ SIngleElementSortedArray.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1) return arr[0];
+        int st = 0, end = n - 1, mid;
+        while(st <= end){
+            mid = st + (end-st)/2;
+            if((mid==0 && arr[0]!=arr[1]) || (mid==n-1 && arr[n-1]!=arr[n-2])){
+                return arr[mid];
+            }
+            if(arr[mid-1] != arr[mid] && arr[mid] != arr[mid+1]){
+                return arr[mid];
+            }
+            if(mid % 2 == 0){
+                if(arr[mid-1] == arr[mid]){
+                    end = mid - 1;
+                }
+                else {
+                    st = mid + 1;
+                }
+            }
+            else {
+                if(arr[mid-1] == arr[mid]){
+                    st = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+
+            }
+        }
+        return -1;
+    }
+};
